9525603f20c7dc539cb87a302b34cc19aec6d7d3
George Ilie <georgeilie@live.com>
2022-08-15 18:31:18 +0100

    add support for gulp.src file array
    if multiple files are supplied in gulp.src, the resx will be individually read and the stringified output of each will be merged into the same json namespace

--- index.js
+++ index.js
@@ -7,39 +7,37 @@ var PluginError = gutil.PluginError;
 const PLUGIN_NAME = 'gulp-resx2js';
 
 function resx2JS(options) {
+	files = [];
 	options = options || {};
+	outputFile = null;
 
 	var stream = through2.obj(function(file, enc, callback) {
-		var content, document, children, childNode, outputObj = {};
-
-		var namespace = options.namespace || '';
-
 		if (file.isNull()) {
 			this.push(file);
 			return callback();
 		}
-
 		if (file.isStream()) {
 			throw new PluginError(PLUGIN_NAME, "Streaming is not supported");
 		}
-
-		content =  file.contents.toString('utf8');
-		document = new xmldoc.XmlDocument(content);
-
-		children = document.childrenNamed('data');
-
-		for(childNode in children) {
-			outputObj[children[childNode].attr.name] = children[childNode].children[0].val;
-		}
-
-		file.path = file.base + (options.target || rext(file.path, '.js'));
-		console.log('Resource file generated: ' + file.base + (options.target || rext(file.path, '.js')));
-
-
+		outputFile = outputFile || file;
+		files.push(file);
+	    return callback();
+    }, function(callback) {
+		var content, children, childNode, outputObj = {}, namespace = options.namespace || '';
+    	files.forEach(function(file) {
+			content = file.contents.toString('utf8');
+			var document = new xmldoc.XmlDocument(content);
+			children = document.childrenNamed('data');
+			for(childNode in children) {
+				outputObj[children[childNode].attr.name] = children[childNode].children[0].val;
+			}
+    	});
 		var resourceJson = JSON.stringify(outputObj);
-		file.contents = new Buffer(namespace == '' ? resourceJson : "{ \"" + namespace + "\": " + resourceJson + "}");
-
-		this.push(file);
+		outputFile.path = outputFile.base + (options.target || rext(outputFile.path, '.js'));
+		outputFile.contents = new Buffer(namespace == '' ? resourceJson : "{ \"" + namespace + "\": " + resourceJson + "}");
+		this.push(outputFile);
+		var outputLog = ('Resource file generated: ' + outputFile.base + (options.target || rext(outputFile.path, '.js'))).replace('/', '\\');
+		console.log(outputLog);
 		return callback();
 	});
 
